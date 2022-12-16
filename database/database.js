ed64d3b8be9e87146598f3fd431eb77eebe62390
Leonardo <62109173+leogbittante@users.noreply.github.com>
2022-12-16 11:41:16 -0300

    Delete database directory

--- database/database.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require("sequelize");
-const connection = new Sequelize('guiaperguntas','root','Nay@1404',{
-    host: 'localhost',
-    dialect: 'mysql'
-});
-
-module.exports = connection;
